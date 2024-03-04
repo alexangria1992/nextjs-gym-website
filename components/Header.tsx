16ca794ffa04cd80657494e99d4cedd6368b4492
Alexandre Angrignon <alex040892@gmail.com>
2024-03-04 23:19:38 +1300

    Scroll functionality using hooks + mobile navigation setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
+  const [headerActive, setheaderActive] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      // detect scroll
+      setheaderActive(window.scrollY > 50);
+    };
+    //add scroll event
+    window.addEventListener("scroll", handleScroll);
+    //clear scroll event
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header
-      className="fixed left-0 border-2 border-yellow-300 right-0 w-full
-     bg-white"
+      className={`${
+        headerActive ? "h-[100px]" : "h-[124px]"
+      } fixed top-0   bg-primary-200  w-full h-[100px] transition-all z-50 max-w-[1920px]`}
     >
-      <div className="container mx-auto border-2 border-green-500">
-        <Nav containerStyles=" py-12 flex gap-4 " />
+      <div className="container  mx-auto h-full flex items-center justify-between ">
+        {/* Logo */}
+        <Link href="">
+          <Image src={"/assets/img/logo.png"} width={117} height={55} alt="" />
+        </Link>
+        {/* Mobile Nav -- hidden on large devices */}
+        <MobileNav containerStyles="text-white xl:hidden" />
+        {/* Desktop Nav - hidden on small devices */}
+        <Nav containerStyles="flex gap-4 text-white hidden xl:flex" />
       </div>
     </header>
   );
